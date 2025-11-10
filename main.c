ad7d0e41145fe93b57b4666a148688002e44fcbe
Ian <ghk.godkiller@gmail.com>
2025-11-10 12:26:10 -0600

    yeet 2

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 typedef struct {
     int x, y;
 } Vector2i;
-// pollos es el mejor del universo si o yeah yeeeeeeetttt 4 letters one word 
+// pollos es el mejor del universo si o yeah yeeeeeeetttt 4 letters one word geee ge yeet
 typedef struct {
     int x, y, w, h;
 } Platform;
